f67279971f01414302fac0ba317ffc29af0ecad5
Khuslen <khuslen.enkhamgalan@student.griffith.ie>
2025-10-20 15:34:48 +0100

    fix: update navigation links in Layout component

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/menu": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/menu.js"
     ]
   },
   "ampFirstPages": []
--- components/Layout.js
+++ components/Layout.js
@@ -6,10 +6,8 @@ export default function Layout({ children }) {
       <nav className="bg-white shadow-md p-4 flex justify-between">
         <h1 className="text-xl font-bold">🍸 My Digital Menu</h1>
         <div className="space-x-4">
-          <Link href="/">Home</Link>
-          <Link href="/menu">Menu</Link>
+          <Link href="/">Menu</Link>
           <Link href="/edit">Edit</Link>
-          <Link href="/about">About</Link>
         </div>
       </nav>
       <main className="p-6">{children}</main>
