88331571b0ce6b0209c294ecacb0ddc0b7a0867a
Khuslen <khuslen.enkhamgalan@student.griffith.ie>
2025-10-23 21:20:01 +0100

    fix: update styling in MenuItem component for consistency

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,9 +1,8 @@
 export default function MenuItem({ name, price }) {
-    return (
-      <div className="bg-white shadow-md rounded-lg p-4 flex justify-between">
-        <span className="font-medium">{name}</span>
-        <span className="text-gray-500">{price}</span>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="flex justify-between bg-white shadow-sm rounded-lg p-3">
+      <span className="font-medium">{name}</span>
+      <span className="text-gray-500">{price}</span>
+    </div>
+  );
+}
